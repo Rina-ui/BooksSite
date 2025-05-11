d3cf7e624415dc0073478344f62213e9042df000
Rina-ui <marinagracia895@gmail.com>
2025-05-11 23:33:14 +0200

    modification

---
+++ Backend/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+
+//connecting to the database
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://m40282897:ma-gra12@cluster0.7q1vrxe.mongodb.net/nom_de_ta_base?retryWrites=true&w=majority&appName=Cluster0',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch((error) => console.error('Connexion a MongoDB échouée !'), error);
+
+app.use(express.json());
+//middleware
+app.use((res, req, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requestred-With, Content-Type, Accept');
+    res.setHeader('Access-Control_Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+})
+
+
+module.exports = app;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,25 @@
 import React from "react";
 
+
 function Login() {
+
+    return (
+        <div>
+        <h1>Login</h1>
+        <fieldset>
+            <legend>Login</legend>
+            <form>
+                <label htmlFor="name">Username</label>
+                <input type="text" id="nmae" name="name" required /><br />
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" name="password" required /><br />
+                <button type="submit">Login</button>
+                <p>Don't you have a compte?</p>
+                <a href="/register">Register</a>
+            </form>
+        </fieldset>
+        </div>
+    );
 }
 
 export default Login;
